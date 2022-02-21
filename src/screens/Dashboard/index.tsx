e02e8b1ce91680f609fd66decb735e52b04c53c7
Charles Ramos <charles.ramos@ext.sinqia.com.br>
2022-02-21 14:09:17 -0300

    feat: Finished Dashboard screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -12,10 +12,57 @@ import {
   UserName,
   Icon,
   HighLightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from "./styles";
 import { HighLightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
+
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "10/04/2022",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: "Alimentação",
+        icon: "coffee",
+      },
+      date: "11/04/2022",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Alugel do Apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag",
+      },
+      date: "06/04/2022",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -55,6 +102,14 @@ export function Dashboard() {
           lastTrasction="01 à 16 de abril"
         />
       </HighLightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components/native";
+import { FlatList, FlatListProps } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize"; //lib para melhorar a proporção
 import { Feather } from "@expo/vector-icons";
+
+import { DataListProps } from ".";
 
 export const Container = styled.View`
   flex: 1;
@@ -60,3 +63,21 @@ export const HighLightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+export const Transactions = styled.View`
+  flex: 1%; /* 1% para pegar o espaço restante da tela*/
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new (
+    props: FlatListProps<DataListProps>
+  ) => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+})``;
